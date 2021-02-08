cb20faa1e37e821505fda24fd22930e6c7d79e79
MELSElearn <78197595+MELSElearn@users.noreply.github.com>
2021-02-08 10:51:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -16,6 +16,16 @@ def form():
 def my_form_post():
     imagefile = flask.request.files.get('imagefile', '')
 
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+    imagefile = flask.request.files.get('imagefile', '')
+    #f = request.files['file']
+    #f.save(secure_filename(f.filename))
+    #return 'file uploaded successfully'
+      
+    
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
